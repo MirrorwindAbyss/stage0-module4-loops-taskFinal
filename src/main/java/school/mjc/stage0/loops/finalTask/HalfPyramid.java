fc65c87661233edb6166246f62258a1c588edd74
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-08-02 10:39:35 +0500

    Done!

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for(int i=0; i<=cathetusLength; i++){
+            for(int space=0; space<=cathetusLength-i; space++){
+                System.out.print(" ");
+            }
 
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for(int i=1; i<10; i++){
+            System.out.println(i*numberTableToPrint);
+        }
+    }
+
+    public static void main(String[] args) {
+        MultiplicationTable m = new MultiplicationTable();
+        m.printTable(3);
     }
 }
