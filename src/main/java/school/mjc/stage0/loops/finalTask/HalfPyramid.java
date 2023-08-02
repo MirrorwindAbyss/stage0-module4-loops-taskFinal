1bea0146092b406f79b59ccca81bd0446217ba57
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-08-02 10:53:16 +0500

    Done!

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-        /*for(int i=0; i<=cathetusLength; i++){
+        for(int i=0; i<=cathetusLength; i++){
             for(int space=0; space<=cathetusLength-i; space++){
                 System.out.print(" ");
             }
@@ -11,18 +11,9 @@ public class HalfPyramid {
                 System.out.print("* ");
             }
             System.out.println();
-        }*/
-
-        for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= cathetusLength; j++) {
-                if (j >= i) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
         }
+
+
     }
 
     public static void main(String[] args) {
